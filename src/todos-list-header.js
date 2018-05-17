23bf7c1c8a3278ed5c4d80a066bb11f28b2110c6
2LazyToCare <39344028+2LazyToCare@users.noreply.github.com>
2018-05-17 14:43:39 +0100

    Update todos-list-header.js
    Acrescentei a validação PropsType.

--- src/todos-list-header.js
+++ src/todos-list-header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 import "./TodoList.css";
 
@@ -53,4 +54,9 @@ export default class TodosListHeader extends React.Component {
       </thead>
     );
   }
+}
+
+TodosListHeader.propTypes = {
+  filter: PropTypes.string,
+  filterTodos: PropTypes.func
 }
