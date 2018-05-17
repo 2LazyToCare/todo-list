01e03ed318d04b5f60915f3ecd0d49ba982ff62e
2LazyToCare <39344028+2LazyToCare@users.noreply.github.com>
2018-05-17 17:15:01 +0100

    Apagar Comentários
    Apaguei algum código que estava comentado.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-
-// ReactDOM.render(<App />, document.getElementById("app"));
-
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import Routers from "./Routers";
+import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
