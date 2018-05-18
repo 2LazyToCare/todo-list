defad3550f8dbe98e2f3df11598cbe5d79d13c91
2LazyToCare <39344028+2LazyToCare@users.noreply.github.com>
2018-05-18 10:13:58 +0100

    Update create-todo.js

--- src/create-todo.js
+++ src/create-todo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import "./TodoList.css";
+import "./CSS/TodoList.css";
 import FaPlusCircle from "react-icons/lib/fa/plus-circle";
 
 export default class CreateTodo extends React.Component {
