af010285ca5d71f7c594e8fa0e92eb648783cb0a
2LazyToCare <39344028+2LazyToCare@users.noreply.github.com>
2018-05-18 10:12:46 +0100

    Update Info.js
    Adding correct css path.

--- src/Info.js
+++ src/Info.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Info.css";
+import "./CSS/Info.css";
 
 export default class Info extends React.Component {
   render() {
